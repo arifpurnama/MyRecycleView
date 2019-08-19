cd1f93664306dc4ceb95e58e1a7d4fb990efef35
arifpurnama <arifpurnama6@gmail.com>
2019-08-19 20:41:12 +0700

    Ini adalah pesan commit pertama anda

---
+++ app/src/main/java/com/creativestoreid/arf/myrecyclerview/MainActivity.java
@@ -0,0 +1,65 @@
+package com.creativestoreid.arf.myrecyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView rvHeroes;
+    private ArrayList<Hero> list = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rvHeroes = findViewById(R.id.rv_hero);
+        rvHeroes.setHasFixedSize(true);
+
+        list.addAll(HeroesData.getListData());
+        showRecyclerList();
+    }
+
+    private void showRecyclerList() {
+        rvHeroes.setLayoutManager(new LinearLayoutManager(this));
+        ListHeroAdapter listHeroAdapter = new ListHeroAdapter(list);
+        rvHeroes.setAdapter(listHeroAdapter);
+    }
+
+    private void showRecycleGrid(){
+        rvHeroes.setLayoutManager(new GridLayoutManager(this,2));
+        GridHeroAdapter gridHeroAdapter = new GridHeroAdapter(list);
+        rvHeroes.setAdapter(gridHeroAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        setMode(item.getItemId());
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setMode(int selectMode) {
+        switch (selectMode) {
+            case R.id.action_list:
+                showRecyclerList();
+                break;
+            case R.id.action_grid:
+                showRecycleGrid();
+                break;
+            case R.id.action_cardview:
+                break;
+        }
+    }
+}
